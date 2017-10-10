885d570f1f26ad2fbc19e96d4b7a2bb99372a653
Shane Richman <Shane.M.Richman@gmail.com>
2017-10-10 00:13:40 +0000

    prints out url of each profile

--- download_avatars.js
+++ download_avatars.js
@@ -1,22 +1,34 @@
 var request = require('request');
-var fs = require('fs');
 console.log("Welcome to the GitHub Avatar Downloader!");
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 var GITHUB_USER = "ShaneRichman";
 var GITHUB_TOKEN = "f4911c5a1d18f7eb9266b91883cce9ac79e91bb4";
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
 console.log(requestURL);
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  request.get('https://sytantris.github.io/http-examples/future.jpg')
-    .on('error', function(err) {
-      throw err;
-    })
-    .on('response', function(response) {
-      console.log("Done Downloading!");
-      console.log('Response Status Code: ', response.statusMessage, response.headers['content-type']);
+  request.get(options, function(err, response, body) {
+    var data = JSON.parse(body);
+    data.forEach(function(profile) {
+      console.log(profile.avatar_url);
     });
+
+
+
+  });
 }
+
+
+var options = {
+  url: requestURL,
+  headers: {
+    'user-agent': GITHUB_USER
+  }
+};
+
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
