00821eb1f6c2881db58b789a410cf0b2fea10541
Shane Richman <Shane.M.Richman@gmail.com>
2017-10-11 00:20:33 +0000

    error handling for missing avatar folder

--- download_avatars.js
+++ download_avatars.js
@@ -2,29 +2,30 @@ if (process.argv.length < 4) {
   console.log("You need a Repo owner and a Repo name in the respective order");
   return;
 }
-
+require('dotenv').config();
 var request = require('request');
 var fs = require('fs');
 console.log("Welcome to the GitHub Avatar Downloader!");
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
-var GITHUB_USER = "ShaneRichman";
-var GITHUB_TOKEN = "f4911c5a1d18f7eb9266b91883cce9ac79e91bb4";
-var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+var requestURL = 'https://' + process.env.DB_GITHUB_USER + ':' + process.env.DB_GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
 function getRepoContributors(repoOwner, repoName, cb) {
   request.get(options, function(err, response, body) {
     var data = JSON.parse(body);
     data.forEach(function(profile) {
+      if (!fs.existsSync("./avatars")) {
+        fs.mkdirSync("./avatars");
+      }
       cb(profile.avatar_url, "avatars/" + profile.login + ".jpg");
     });
   });
 }
 
-var options = {
+const options = {
   url: requestURL,
   headers: {
-    'user-agent': GITHUB_USER
+    'user-agent': process.env.DB_GITHUB_USER
   }
 };
 
